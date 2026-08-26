1159f04cfc940d797af44cc269333f6a2caf986d
louri-neto <lourivalneto456@gmail.com>
2026-08-25 22:45:39 -0300

    Update main.c

--- main.c
+++ main.c
@@ -7,12 +7,12 @@
 #endif
 
 int main() {
-#ifdef _WIN32
-    SetConsoleCP(65001);
-    SetConsoleOutputCP(65001);
-#else
-    setlocale(LC_ALL, "");
-#endif
+    #ifdef _WIN32
+        SetConsoleCP(65001);
+        SetConsoleOutputCP(65001);
+    #else
+        setlocale(LC_ALL, "");
+    #endif
 
     return 0;
 }
