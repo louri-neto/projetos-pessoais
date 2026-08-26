c18f3cc174f61ddc7e6e14080ae650505b83b261
louri-neto <lourivalneto456@gmail.com>
2026-08-25 22:42:22 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <locale.h>
+#endif
+
+int main() {
+#ifdef _WIN32
+    SetConsoleOutputCP(65001);
+#else
+    setlocale(LC_ALL, "");
+#endif
+
+    return 0;
+}
